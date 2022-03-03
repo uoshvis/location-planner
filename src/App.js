d350675cc72b327939e80a25155c8e4934e0e13c
uoshvis <uoshvis@gmail.com>
2022-03-03 19:58:40 +0200

    add initial Calendar component with events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import React from 'react';
+import { Calendar, Views, momentLocalizer} from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import events from './events';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+require('moment/locale/lt.js')
 
-    </div>
-  );
+const localizer = momentLocalizer(moment)
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      events,      
+    }
+
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Calendar
+          selectable
+          localizer={localizer}
+          style={{ height: 500 }}
+          events={this.state.events}
+          defaultView={Views.MONTH}
+          defaultDate={new Date(2015, 3, 12)}
+        />
+      </div>
+    );
+
+  }
+
 }
 
 export default App;

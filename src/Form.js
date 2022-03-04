48aa63216c4f42455b4865e1d7e14eb472b2dbd4
uoshvis <uoshvis@gmail.com>
2022-03-04 13:31:56 +0200

    extract Form component

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,16 @@
+
 function Form(props) {
-    return <h1>Hello, {props.name}</h1>;
+
+  return (
+    <form onSubmit={props.handleSubmitForm}>
+      <label>
+        Event Title:
+        <input type="text" value={props.title} onChange={props.handleTitleChange} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );      
   }
 
 
-export default Form
+export default Form;  

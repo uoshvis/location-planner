7be01cdb181578d4e6fdcd98201cbb6ba2efe2e9
uoshvis <uoshvis@gmail.com>
2022-06-02 15:30:40 +0300

    change mock data

---
+++ src/fakeAPI/mockEvents.js
@@ -0,0 +1,41 @@
+const now = new Date()
+
+const events = [
+    
+
+  {
+    id: 1,
+    location: 'loc1',
+    title: 'Today_loc1',
+    start: new Date(new Date().setHours(new Date().getHours() + 24)),
+    end: new Date(new Date().setHours(new Date().getHours() + 27)),
+  },
+  
+  {
+    id: 2,
+    location: 'loc1',
+    title: 'Point in Time Event loc1',
+    start: now,
+    end: now,
+  },
+
+  {
+    id: 3,
+    title: 'Today_loc2',
+    location: 'loc2',
+    start: new Date(new Date().setHours(new Date().getHours() - 40)),
+    end: new Date(new Date().setHours(new Date().getHours() - 38)),
+  },
+
+  {
+    id: 4,
+    location: 'loc2',
+    title: 'Point in Time Event loc2',
+    start: now,
+    end: now,
+  },
+    
+]
+
+
+module.exports = events
